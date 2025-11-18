7d6cbe407cc7ddf5f4af2a9f1759d1ab5d035474
Bartek Jaruga <245827@edu.p.lodz.pl>
2025-11-18 23:25:58 +0100

    feat: - changed name NoCreatedFundsInformation to NothingToShowInformation and some classnames - added bento template for CreatedFundsPage (just layout)

--- src/pages/CreatedFundsPage/CreatedFundsPage.tsx
+++ src/pages/CreatedFundsPage/CreatedFundsPage.tsx
@@ -5,18 +5,27 @@ export function CreatedFundsPage() {
   return (
     <>
       <div className={styles['page']}>
-        <NoCreatedFundsInformation />
+        {/* <NothingToShowInformation /> */}
+        <div className={styles['grid-container']}>
+          <div className={styles['grid-container__classes']}>Classes</div>
+          <div className={styles['grid-container__create-fund']}>
+            Create fund
+          </div>
+          <div className={styles['grid-container__fund-list']}>Fund list</div>
+          <div className={styles['grid-container__class-info']}>Class info</div>
+          <div className={styles['grid-container__event-log']}>Event log</div>
+        </div>
       </div>
     </>
   );
 }
 
-function NoCreatedFundsInformation() {
+function NothingToShowInformation() {
   return (
     <>
-      <div className={styles['information-wrapper']}>
-        <PackageOpen className={styles['information-wrapper__icon']} />
-        <p className={styles['information-wrapper__text']}>
+      <div className={styles['nothing-to-show-info-wrapper']}>
+        <PackageOpen className={styles['nothing-to-show-info-wrapper__icon']} />
+        <p className={styles['nothing-to-show-info-wrapper__text']}>
           You are not a treasurer in any of your classes
         </p>
       </div>
