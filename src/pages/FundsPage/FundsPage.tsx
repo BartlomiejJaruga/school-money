44c598ebd6f575291668753bcccc1a4cda225868
BartlomiejJaruga <245827@edu.p.lodz.pl>
2025-11-23 23:58:17 +0100

    feat: - Created bento grid template for FundsPage - Changed a little background colors in app (a little darker for better contrast of whole app)

--- src/pages/FundsPage/FundsPage.tsx
+++ src/pages/FundsPage/FundsPage.tsx
@@ -4,15 +4,10 @@ export function FundsPage() {
   return (
     <>
       <div className={styles['page']}>
-        <div className={styles['first-col']}>
-          <h1>Hello</h1>
-          <h1>Hello</h1>
-          <h1>Hello</h1>
-        </div>
-        <div className={styles['second-col']}>
-          <h2>World</h2>
-          <h2>World</h2>
-          <h2>World</h2>
+        <div className={styles['grid-container']}>
+          <div className={styles['grid-container__fund-list']}>fund-list</div>
+          <div className={styles['grid-container__children']}>children</div>
+          <div className={styles['grid-container__history']}>history</div>
         </div>
       </div>
     </>
