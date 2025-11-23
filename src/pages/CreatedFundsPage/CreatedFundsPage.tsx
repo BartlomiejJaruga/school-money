720473af8a82cccf4280e539b4171eb3f4833a11
BartlomiejJaruga <245827@edu.p.lodz.pl>
2025-11-23 15:26:41 +0100

    feat: - Added classes section (with buttons) to CreatedFundsPage bento grid - Added create fund button to create fund section in CreatedFundsPage bento grid - Added fund list (top bar with summary and buttons for changing showed funds) to CreatedFundsPage bento grid - Added FundTile with information about fund (missing progress bars) - Added variable for setting the bento grid gap on CreatedFundsPage - Added proper styling to every element added in this commit - Added default-fund image - Applied default font and removed padding from default-button mixin - Added info-tile mixin for small tiles with info message about sth - Added a variable for background of inactive buttons that are clickable elements of layout (e.g. buttons for selecting current showed class in CreatedFundsPage) - Changed value of $color-subtext-gray variable

--- src/pages/CreatedFundsPage/CreatedFundsPage.tsx
+++ src/pages/CreatedFundsPage/CreatedFundsPage.tsx
@@ -1,5 +1,8 @@
 import styles from './CreatedFundsPage.module.scss';
-import { PackageOpen } from 'lucide-react';
+import clsx from 'clsx';
+import { PackageOpen, Ticket, TicketCheck, TicketX } from 'lucide-react';
+import defaultFundPhoto from '@assets/default-fund.jpg';
+import { useState } from 'react';
 
 export function CreatedFundsPage() {
   return (
@@ -7,11 +10,21 @@ export function CreatedFundsPage() {
       <div className={styles['page']}>
         {/* <NothingToShowInformation /> */}
         <div className={styles['grid-container']}>
-          <div className={styles['grid-container__classes']}>Classes</div>
-          <div className={styles['grid-container__create-fund']}>
-            Create fund
+          <div className={styles['grid-container__classes']}>
+            <ClassButton name="1A 22/23" active={true} />
+            <ClassButton name="3C 20/21" />
+            <ClassButton name="4B 19/20" />
           </div>
-          <div className={styles['grid-container__fund-list']}>Fund list</div>
+          <button className={styles['grid-container__create-fund']}>
+            Create fund
+          </button>
+          <div className={styles['grid-container__fund-list']}>
+            <FundListTopBar />
+            <div className={styles['fund-list__container']}>
+              <FundTile />
+              <FundTile />
+            </div>
+          </div>
           <div className={styles['grid-container__class-info']}>Class info</div>
           <div className={styles['grid-container__event-log']}>Event log</div>
         </div>
@@ -30,5 +43,92 @@ function NothingToShowInformation() {
         </p>
       </div>
     </>
+  );
+}
+
+type ClassButtonProps = {
+  name: string;
+  active?: boolean; // when connecting to backend change this
+};
+
+function ClassButton({ name, active = false }: ClassButtonProps) {
+  const [isActive, setIsActive] = useState(active);
+
+  return (
+    <button
+      className={clsx(
+        styles['classes__button'],
+        isActive && styles['classes__button--active']
+      )}
+      onClick={() => setIsActive(!isActive)}
+    >
+      {name}
+    </button>
+  );
+}
+
+function FundListTopBar() {
+  return (
+    <div className={styles['fund-list__top-bar']}>
+      <div className={styles['top-bar__funds-summary']}>
+        <span>Total funds available</span>
+        <h2>2100 PLN</h2>
+        <span>in 10 funds</span>
+      </div>
+      <div
+        className={clsx(
+          styles['top-bar__funds-type-button'],
+          styles['top-bar__funds-type-button--active']
+        )}
+      >
+        <Ticket />
+        <span>Active</span>
+      </div>
+      <div className={styles['top-bar__funds-type-button']}>
+        <TicketX />
+        <span>Refunded</span>
+      </div>
+      <div className={styles['top-bar__funds-type-button']}>
+        <TicketCheck />
+        <span>Finished</span>
+      </div>
+    </div>
+  );
+}
+
+function FundTile() {
+  return (
+    <div className={styles['fund-tile']}>
+      <img
+        src={defaultFundPhoto}
+        alt="fund photo"
+        className={styles['fund-tile__photo']}
+      />
+      <div className={styles['fund-tile__details']}>
+        <div className={styles['details__top']}>
+          <div>
+            <h2 className={styles['fund-title']}>Theater trip</h2>
+            <p className={styles['fund-description']}>
+              After the play ends, the theater will self-ignite in an act of
+              despair and dramatic events.
+            </p>
+          </div>
+          <div>
+            <span>Funds available</span>
+            <h2>24 PLN</h2>
+          </div>
+        </div>
+        <div>Time</div>
+        <div>Budget</div>
+        <div className={styles['details__actions-bar']}>
+          <button className={styles['actions-bar__withdraw']}>
+            Withdraw money
+          </button>
+          <button className={styles['actions-bar__more-info']}>
+            More info
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
