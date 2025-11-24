4aaedaa612fa1616a2a9febaa078218602a280cd
BartlomiejJaruga <245827@edu.p.lodz.pl>
2025-11-24 23:58:25 +0100

    feat: - Added pagination with styling to FundsPage

--- src/pages/FundsPage/FundsPage.tsx
+++ src/pages/FundsPage/FundsPage.tsx
@@ -1,11 +1,36 @@
 import styles from './FundsPage.module.scss';
+import { MoveLeft, MoveRight } from 'lucide-react';
+import clsx from 'clsx';
 
 export function FundsPage() {
   return (
     <>
       <div className={styles['page']}>
         <div className={styles['grid-container']}>
-          <div className={styles['grid-container__fund-list']}>fund-list</div>
+          <div className={styles['grid-container__fund-list']}>
+            <div className={styles['pagination']}>
+              <button className={styles['pagination__prev']} disabled={true}>
+                <MoveLeft />
+                Prev
+              </button>
+              <div className={styles['pagination__pages']}>
+                <button
+                  className={clsx(
+                    styles['pages__page'],
+                    styles['pages__page--active']
+                  )}
+                >
+                  1
+                </button>
+                <button className={styles['pages__page']}>2</button>
+                <button className={styles['pages__page']}>3</button>
+              </div>
+              <button className={styles['pagination__next']}>
+                Next
+                <MoveRight />
+              </button>
+            </div>
+          </div>
           <div className={styles['grid-container__children']}>children</div>
           <div className={styles['grid-container__history']}>history</div>
         </div>
