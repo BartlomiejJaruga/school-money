2433a1db88d3b2d99f4a84ca6937b059330cae0f
BartlomiejJaruga <245827@edu.p.lodz.pl>
2025-12-08 02:09:17 +0100

    feat: - Added avatar section with proper styling to ProfilePage bento grid - Added overlay hover effect for future changing avatar functionality

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,11 +1,15 @@
+import { ImagePlus } from 'lucide-react';
 import styles from './ProfilePage.module.scss';
+import defaultUserPhoto from '@assets/default-user.png';
 
 export function ProfilePage() {
   return (
     <>
       <div className={styles['page']}>
         <div className={styles['grid-container']}>
-          <div className={styles['grid-container__avatar']}>Avatar</div>
+          <div className={styles['grid-container__avatar']}>
+            <Avatar />
+          </div>
           <div className={styles['grid-container__basic-info-form']}>
             Basic info form
           </div>
@@ -15,5 +19,20 @@ export function ProfilePage() {
         </div>
       </div>
     </>
+  );
+}
+
+function Avatar() {
+  return (
+    <div className={styles['avatar-wrapper']}>
+      <img
+        src={defaultUserPhoto}
+        alt="avatar photo"
+        className={styles['avatar__photo']}
+      />
+      <div className={styles['avatar__overlay']}>
+        <ImagePlus />
+      </div>
+    </div>
   );
 }
